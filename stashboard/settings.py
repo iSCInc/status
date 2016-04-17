4f5aa0036d71ddfa533c8b6c88f49dd52fd82471
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-04-17 13:28:21 +0200

    Edit TWITTER_HANDLE

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -5,14 +5,14 @@ DEBUG = False
 SITE_NAME = "Status | iSC Inc."
 SITE_AUTHOR = "Suriyaa Kudo"
 SITE_URL = "http://iscinc-status.appspot.com"
-REPORT_URL = "mailto:contact@inc.isc"
+REPORT_URL = "mailto:status@inc.isc"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_TOKEN_SECRET = ''
-TWITTER_HANDLE = 'iscinc_status'
+TWITTER_HANDLE = 'iscincstatus'
 
 # RSS Feed settings
 RSS_NUM_EVENTS_TO_FETCH = 50
